ac89fee5bc6c46eaa77841bc199007e67b1f8370
Alexandre Gallego Mato <alexandre.gallego.mato@gmail.com>
2025-11-03 23:31:05 +0100

    Added changes to api.py file

--- finperf/src/finperf/api.py
+++ finperf/src/finperf/api.py
@@ -1,17 +1,21 @@
-from abc import ABC, abstractmethod
 from .company import Company
+import requests #type: ignore
 
-class ApiInterface(ABC):
-    @abstractmethod
-    def get(self):
-        pass
-
-class BalanceSheetApi(ApiInterface):
-
+class CompanyApi:
     def __init__(self, url: str, apiKey: str, company: Company):
         self.url = url
-        self._apiKey = apiKey
+        self.apiKey = apiKey
         self.company = company
 
     def get(self):
-        return None
+        response = requests.get(self.url)
+        return response.json()
+
+class BalanceSheetApi(CompanyApi):
+
+    def __init__(self, apiKey: str, company: Company, url: str = 'ttps://www.alphavantage.co/query?function=BALANCE_SHEET&symbol='):
+        super().__init__(
+            url=url + f'{company.ticker}' + f'&apikey={apiKey}',
+            apiKey=apiKey,
+            company=company
+        )
